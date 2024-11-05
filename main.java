49ae7b7cae7c34f1fe793f9ffbb5188de283eb01
Rash2508 <117517108+Rash2508@users.noreply.github.com>
2024-11-05 21:31:33 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,10 @@
+public class main {
+	public static void main(String[] args) {
+		Entry1 obj1 = new Entry1();
+		//Reg1 obj2=new Reg1();
+		//Smsg obj3=new Smsg();
+		//Dmsg obj4=new Dmsg();
+		
+		
+	}
+}
